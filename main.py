35192dd27c096be7b3e0ede1758e49b51f146f6d
cubomx <cheservelram657>
2020-05-02 13:24:45 -0500

    UPDATE: Fixed list out of range, still getting some repeated values
    Signed-off-by: cubomx <cheservelram657>

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 from kdtree import kdtree, Node
 from math import ceil
 from copy import deepcopy as dp
-SIZE = 1
+SIZE = 0
 MAX_DIMENSION = 0
 
 def printInorder(root):
@@ -22,7 +22,10 @@ def orderByLevel(node, sortedNodes, isRight, level):
         level_ = 0
     s = sorted(sortedNodes, key=lambda x: x.key[level_])
     median = ceil(len(sortedNodes) / 2) - 1
-    print(sortedNodes[median].key)
+
+    for i in sortedNodes:
+        print(i.key)
+    print("\n\n")
     node.isLeaf = False
     if isRight:
         node.right = sortedNodes[median]
@@ -31,8 +34,16 @@ def orderByLevel(node, sortedNodes, isRight, level):
         node.left = sortedNodes[median]
         node = node.left
     if len(sortedNodes) > 1:
-        orderByLevel(node, sortedNodes[0:median+1], False, level_ + 1)
-        orderByLevel(node, sortedNodes[median + 1:], True, level_ + 1)
+        if median > 1:
+            orderByLevel(node, sortedNodes[0:median], False, level_ + 1)
+        else:
+            SIZE += 1
+            node.left = sortedNodes[0]
+        if len(sortedNodes) - median > 1:
+            orderByLevel(node, sortedNodes[median + 1:], True, level_ + 1)
+        else:
+            SIZE += 1
+            node.right = sortedNodes[0]
     return
 
 
@@ -43,7 +54,7 @@ def orderTree(sortedNodes, tree):
     tree = kdtree(sortedNodes[median])
     orderByLevel(tree.root, sortedNodes[0:median], False, 1)
     orderByLevel(tree.root, sortedNodes[median+1:], True, 1)
-
+    print(SIZE)
     return tree
 
 def orderNodes(nodes, tree):
