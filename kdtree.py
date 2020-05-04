dea6d414a46fcc3e88723d1d92fc0fc26d1b3a39
cubomx <cheservelram657>
2020-05-03 19:05:40 -0500

    UPDATE: Delete print function of K-D Tree
    Signed-off-by: cubomx <cheservelram657>

--- kdtree.py
+++ kdtree.py
@@ -2,17 +2,6 @@ class kdtree():
     def __init__(self, root):
         self.root = root
 
-
-
-    def printTree(self):
-        root = self.root
-        if not root.isLeaf:
-            if not root.left is None:
-                self.search(root.left)
-            if not root.right is None:
-                self.search(root.right)
-
-
 class Node():
     def __init__(self, values):
         self.key = values #it must be an array (if you want more level search)
